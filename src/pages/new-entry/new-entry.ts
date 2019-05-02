6126eb2ff3945f11f6f80bd09a57d19c8eecf3fd
Bruno Carlos Mota Araujo <BrunoAraujo@caesb.gov.br>
2019-05-02 17:33:25 -0300

    implementado funcionalidade de salvar no BD

--- src/pages/new-entry/new-entry.ts
+++ src/pages/new-entry/new-entry.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
+
 @IonicPage()
 @Component({
   selector: 'page-new-entry',
@@ -14,6 +16,7 @@ export class NewEntryPage {
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
+    public sqlite: SQLite,
     private builder: FormBuilder) {
 
       this.entryForm = builder.group({
@@ -27,6 +30,7 @@ export class NewEntryPage {
   submitForm() {
     console.log('Enviando dados..');
     console.log(JSON.stringify(this.entry));
+    this.insertBD();
     this.navCtrl.pop();
   }
 
@@ -34,4 +38,30 @@ export class NewEntryPage {
     console.log('Cancelando...');
     this.navCtrl.pop();
   }
+
+  insertBD() {
+    console.log('início da gravação do BD');
+
+    this.sqlite.create({
+      name: 'data.db',
+      location: 'default'
+    })
+    .then((db: SQLiteObject) => {
+      console.log('bd criado');
+
+      db.sqlBatch([
+        "CREATE TABLE IF NOT EXISTS entries (id INTEGER PRIMARY KEY AUTOINCREMENT, amount DECIMAL, description TEXT)"
+      ])
+      .then(() => {
+        const sql = "INSERT INTO entries (amount) VALUES (?)";
+        const data = [this.entry['amount']];
+
+        return db.executeSql(sql, data)
+          .then(() => console.log('insert realizado com sucesso'))
+          .catch(e => console.error('erro ao inserir na tabela', JSON.stringify(e)));
+      })
+      .catch(e => console.error('erro ao criar a tabela', JSON.stringify(e)));
+    });
+  }
 }
+
